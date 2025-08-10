16f7e1d705fcc53dfb2f541f9193cf6a3f736aa1
erostho <erostho1202@gmail.com>
2025-08-10 21:40:16 +0700

    Update main.py

--- main.py
+++ main.py
@@ -76,14 +76,14 @@ except Exception as e:
 # ========== THAM SỐ KỸ THUẬT ==========
 TP_MULTIPLIER = 1.5
 SL_MULTIPLIER = 1.0
-ADX_THRESHOLD = 12
+ADX_THRESHOLD = 10
 K_ATR_SL = 0.6   # tối thiểu SL = max(SL_MIN_PCT, 0.6*ATR/entry)
 K_ATR_TP = 1.2   # tối thiểu TP = max(TP_MIN_{RELAX|STRICT}, 1.2*ATR/entry)
 COINS_LIMIT = 300  # Số coin phân tích mỗi lượt
 # ===== CONFIG =====
-SL_MIN_PCT   = 0.005    # SL tối thiểu 0.7%
-TP_MIN_RELAX = 0.015     # TP tối thiểu 3% (RELAX)
-TP_MIN_STRICT= 0.05     # TP tối thiểu 5% (STRICT)
+SL_MIN_PCT   = 0.00    # SL tối thiểu 0.7%
+TP_MIN_RELAX = 0.00     # TP tối thiểu 3% (RELAX)
+TP_MIN_STRICT= 0.00     # TP tối thiểu 5% (STRICT)
 TOPN_PER_BATCH = 10   # tuỳ bạn, 5/10/15...
 SL_MIN_PCT_BASE = SL_MIN_PCT
 TP_MIN_RELAX_BASE = TP_MIN_RELAX
