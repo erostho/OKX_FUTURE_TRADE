960da01d2453fd1a5f7e8acc567833337b36ef77
erostho <erostho1202@gmail.com>
2025-12-06 18:10:56 +0700

    Update main.py

--- main.py
+++ main.py
@@ -380,7 +380,7 @@ def is_backtest_time_vn():
     m = now_vn.minute
 
     # các lần cron full bot đang chạy ở phút 5,20,35,50
-    if h in (9, 17, 20) and 5 <= m <= 59:
+    if h in (9, 15, 18) and 5 <= m <= 59:
         return True
     if h == 22 and 50 <= m <= 59:
         return True
